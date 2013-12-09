6b4323a4b30239174622c90b8b50ed69b8dde9e7
Leboucetmistere <arthurdp96@hotmail.fr>
2013-12-09 20:48:04 +0100

    Commit .gitignore

---
+++ SGF/SGF/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  SGF
+//
+//  Created by DEPASSE Arthur on 09/12/2013.
+//  Copyright (c) 2013 DEPASSE Arthur. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
